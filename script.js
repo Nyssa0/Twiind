cc63090092804ede54ab03df09fbc344e808685a
Nyssa0 <hassinaayachi@hotmail.com>
2025-03-05 10:12:16 +0100

    ✨ Add shuffle and hidden

--- script.js
+++ script.js
@@ -37,52 +37,55 @@ async function displayPokemons() {
             const backgroundClass = getBackgroundClass(randomPokemon.type);
 
             pokemonElement.innerHTML = `
-                <div class="pokemon__background pokemon__background--${backgroundClass}">
-                    <table class="pokemon__header">
-                        <tr>
-                            <td class="basic" colspan="3">Basic Pokémon</td>
-                        </tr>
-                        <tr>
-                            <td class="pokemon__name">${randomPokemon.name} ${randomPokemon.type}</td>
-                            <td class="pokemon__hp">${randomPokemon.hp} HP</td>
-                            <td class="typesign"> ☻ </td>
-                        </tr>
-                    </table>
-                    <img src="${randomPokemon.image}" alt="${randomPokemon.name}">
-                    <br>
-                    <div class="pokemon__info">
-                        <p class="pokemon__description">This is a pokemon description.</p>
-                        <table class="pokemon__stats">
+                <div class="pokemon__card-inner">
+                    <div class="pokemon__background pokemon__background--${backgroundClass}">
+                        <table class="pokemon__header">
                             <tr>
-                                <td class="pokemon__energy">☻</td>
-                                <td class="pokemon__attack_description pokemon__attack_description--center"><span class="label">Gnaw</span></td>
-                                <td class="pokemon__damage">10</td>
+                                <td class="basic" colspan="3">Basic Pokémon</td>
                             </tr>
-                        </table>
-                        <table class="pokemon__stats">
                             <tr>
-                                <td class="pokemon__energy">☻ ☻</td>
-                                <td class="pokemon__attack_description"><span class="label">Thundershock</span> <span class="labeltext">Flip a coin. If tails, ${randomPokemon.name} does 10 damage to itself.</span></td>
-                                <td class="pokemon__damage">20</td>
+                                <td class="pokemon__name">${randomPokemon.name} ${randomPokemon.type}</td>
+                                <td class="pokemon__hp">${randomPokemon.hp} HP</td>
+                                <td class="typesign"> ☻ </td>
                             </tr>
                         </table>
-                        <table class="pokemon__costs">
-                            <tr class="pokemon__cost_headers">
-                                <td>weakness</td>
-                                <td>resistance</td>
-                                <td>retreat cost</td>
-                            </tr>
-                            <tr class="pokemon__cost_icons">
-                                <td>☻</td>
-                                <td></td>
-                                <td>☻</td>
-                            </tr>
-                        </table>
-                        <ul>
-                            <li class="italicize">I'm writing about pokemons.</li>
-                            <li class="pokemon__copyrights"><br><span class="strong">Illus Mitsuhiro Arita</strong>  <span class="medium">©1995, 96, 98 Nintendo Creatures, GAMEFREAK ©1999 Wizards</span> <span class="strong">58/102 ●</strong></li>       
-                        </ul>
+                        <img class="pokemon__image" src="${randomPokemon.image}" alt="${randomPokemon.name}">
+                        <br>
+                        <div class="pokemon__info">
+                            <p class="pokemon__description">This is a pokemon description.</p>
+                            <table class="pokemon__stats">
+                                <tr>
+                                    <td class="pokemon__energy">☻</td>
+                                    <td class="pokemon__attack_description pokemon__attack_description--center"><span class="label">Gnaw</span></td>
+                                    <td class="pokemon__damage">10</td>
+                                </tr>
+                            </table>
+                            <table class="pokemon__stats">
+                                <tr>
+                                    <td class="pokemon__energy">☻ ☻</td>
+                                    <td class="pokemon__attack_description"><span class="label">Thundershock</span> <span class="labeltext">Flip a coin. If tails, ${randomPokemon.name} does 10 damage to itself.</span></td>
+                                    <td class="pokemon__damage">20</td>
+                                </tr>
+                            </table>
+                            <table class="pokemon__costs">
+                                <tr class="pokemon__cost_headers">
+                                    <td>weakness</td>
+                                    <td>resistance</td>
+                                    <td>retreat cost</td>
+                                </tr>
+                                <tr class="pokemon__cost_icons">
+                                    <td>☻</td>
+                                    <td></td>
+                                    <td>☻</td>
+                                </tr>
+                            </table>
+                            <ul>
+                                <li class="italicize">I'm writing about pokemons.</li>
+                                <li class="pokemon__copyrights"><br><span class="strong">Illus Mitsuhiro Arita</strong>  <span class="medium">©1995, 96, 98 Nintendo Creatures, GAMEFREAK ©1999 Wizards</span> <span class="strong">58/102 ●</strong></li>       
+                            </ul>
+                        </div>
                     </div>
+                    <img class="pokemon__back is-hidden" src="/assets/back-pokemon-card.png" alt="pokemon card back">
                 </div>
             `;
             randomPokemonContainer.appendChild(pokemonElement);
@@ -92,52 +95,55 @@ async function displayPokemons() {
             const pokemonElement = document.createElement("li");
             pokemonElement.classList.add("pokemon__card");
             pokemonElement.innerHTML = `
-                <div class="pokemon__background pokemon__background--yellow">
-                    <table class="pokemon__header">
-                        <tr>
-                            <td class="basic" colspan="3">Basic Pokémon</td>
-                        </tr>
-                        <tr>
-                            <td class="pokemon__name">${evolvedPokemon.name} </td>
-                            <td class="pokemon__hp">${evolvedPokemon.hp} HP</td>
-                            <td class="typesign"> ☻ </td>
-                        </tr>
-                    </table>
-                    <img src="${evolvedPokemon.image}" alt="${evolvedPokemon.name}">
-                    <br>
-                    <div class="pokemon__info">
-                        <p class="pokemon__description">This is a pokemon description.</p>
-                        <table class="pokemon__stats">
+                <div class="pokemon__card-inner">
+                    <div class="pokemon__background pokemon__background--yellow">
+                        <table class="pokemon__header">
                             <tr>
-                                <td class="pokemon__energy">☻</td>
-                                <td class="pokemon__attack_description pokemon__attack_description--center"><span class="label">Gnaw</span></td>
-                                <td class="pokemon__damage">10</td>
+                                <td class="basic" colspan="3">Basic Pokémon</td>
                             </tr>
-                        </table>
-                        <table class="pokemon__stats">
                             <tr>
-                                <td class="pokemon__energy">☻ ☻</td>
-                                <td class="pokemon__attack_description"><span class="label">Thundershock</span> <span class="labeltext">Flip a coin. If tails, ${evolvedPokemon.name} does 10 damage to itself.</span></td>
-                                <td class="pokemon__damage">20</td>
+                                <td class="pokemon__name">${evolvedPokemon.name} </td>
+                                <td class="pokemon__hp">${evolvedPokemon.hp} HP</td>
+                                <td class="typesign"> ☻ </td>
                             </tr>
                         </table>
-                        <table class="pokemon__costs">
-                            <tr class="pokemon__cost_headers">
-                                <td>weakness</td>
-                                <td>resistance</td>
-                                <td>retreat cost</td>
-                            </tr>
-                            <tr class="pokemon__cost_icons">
-                                <td>☻</td>
-                                <td></td>
-                                <td>☻</td>
-                            </tr>
-                        </table>
-                        <ul>
-                            <li class="italicize">I'm writing about pokemons.</li>
-                            <li class="pokemon__copyrights"><br><span class="strong">Illus Mitsuhiro Arita</strong>  <span class="medium">©1995, 96, 98 Nintendo Creatures, GAMEFREAK ©1999 Wizards</span> <span class="strong">58/102 ●</strong></li>       
-                        </ul>
+                        <img class="pokemon__image" src="${evolvedPokemon.image}" alt="${evolvedPokemon.name}">
+                        <br>
+                        <div class="pokemon__info">
+                            <p class="pokemon__description">This is a pokemon description.</p>
+                            <table class="pokemon__stats">
+                                <tr>
+                                    <td class="pokemon__energy">☻</td>
+                                    <td class="pokemon__attack_description pokemon__attack_description--center"><span class="label">Gnaw</span></td>
+                                    <td class="pokemon__damage">10</td>
+                                </tr>
+                            </table>
+                            <table class="pokemon__stats">
+                                <tr>
+                                    <td class="pokemon__energy">☻ ☻</td>
+                                    <td class="pokemon__attack_description"><span class="label">Thundershock</span> <span class="labeltext">Flip a coin. If tails, ${evolvedPokemon.name} does 10 damage to itself.</span></td>
+                                    <td class="pokemon__damage">20</td>
+                                </tr>
+                            </table>
+                            <table class="pokemon__costs">
+                                <tr class="pokemon__cost_headers">
+                                    <td>weakness</td>
+                                    <td>resistance</td>
+                                    <td>retreat cost</td>
+                                </tr>
+                                <tr class="pokemon__cost_icons">
+                                    <td>☻</td>
+                                    <td></td>
+                                    <td>☻</td>
+                                </tr>
+                            </table>
+                            <ul>
+                                <li class="italicize">I'm writing about pokemons.</li>
+                                <li class="pokemon__copyrights"><br><span class="strong">Illus Mitsuhiro Arita</strong>  <span class="medium">©1995, 96, 98 Nintendo Creatures, GAMEFREAK ©1999 Wizards</span> <span class="strong">58/102 ●</strong></li>       
+                            </ul>
+                        </div>
                     </div>
+                    <img class="pokemon__back is-hidden" src="/assets/back-pokemon-card.png" alt="pokemon card back">
                 </div>
             `;
             evolvedPokemonContainer.appendChild(pokemonElement);
@@ -148,3 +154,22 @@ async function displayPokemons() {
 }
 
 document.addEventListener("DOMContentLoaded", displayPokemons);
+
+function shuffleCards() {
+    const cards = document.querySelectorAll(".pokemon__card");
+    cards.forEach((card) => {
+        card.style.order = Math.floor(Math.random() * cards.length);
+    });
+}
+
+function hideCards() {
+    const cards = document.querySelectorAll(".pokemon__card");
+    cards.forEach((card) => {
+        card.classList.toggle("is-hidden");
+        shuffleCards();
+    });
+}
+
+setInterval(() => {
+    hideCards();
+}, 5000);
