bf04473f16eb3b41e26655d0547b0c4cab228e0a
YesIDo49 <mehdialsidcheikh@gmail.com>
2025-03-06 14:10:17 +0100

    ✨ promise before shuffle cards

--- client.js
+++ client.js
@@ -130,81 +130,87 @@ function getBackgroundClass(type) {
 
 export async function displayPokemons(pokemons) {
     const randomPokemonContainer = document.querySelector(".randomPokemons");
+    const cardPromises = [];
 
     for (const randomPokemon of pokemons) {
-        const pokemonElement = document.createElement("li");
-        pokemonElement.classList.add("pokemon__card");
-        const backgroundClass = getBackgroundClass(randomPokemon.type);
+        cardPromises.push(new Promise(async (resolve) => {
+            const pokemonElement = document.createElement("li");
+            pokemonElement.classList.add("pokemon__card");
+            const backgroundClass = getBackgroundClass(randomPokemon.type);
 
-        const tcgCard = await getTcgCard(randomPokemon.name)
-        console.log(tcgCard);
+            const tcgCard = await getTcgCard(randomPokemon.name);
+            console.log(tcgCard);
 
-        if (tcgCard) {
-            pokemonElement.innerHTML = `
-                <div class="pokemon__card-inner">
-                    <div class="pokemon__background pokemon__background--${backgroundClass}">
-                        <img src="${tcgCard}" class="tcg-card" alt="${randomPokemon.name}">
-                    </div>
-                    <img class="pokemon__back is-hidden" src="/assets/back-pokemon-card.png" alt="pokemon card back">
-                </div>
-            `;
-        } else {
-            pokemonElement.innerHTML = `
-                <div class="pokemon__card-inner">
-                    <div class="pokemon__background pokemon__background--${backgroundClass} --not-tcg">
-                        <table class="pokemon__header">
-                            <tr>
-                                <td class="basic" colspan="3">Basic Pokémon</td>
-                            </tr>
-                            <tr>
-                                <td class="pokemon__name">${randomPokemon.name} ${randomPokemon.type}</td>
-                                <td class="pokemon__hp">${randomPokemon.hp} HP</td>
-                                <td class="typesign"> ☻ </td>
-                            </tr>
-                        </table>
-                        <img class="pokemon__image" src="${randomPokemon.image}" alt="${randomPokemon.name}">
-                        <br>
-                        <div class="pokemon__info">
-                            <p class="pokemon__description">This is a pokemon description.</p>
-                            <table class="pokemon__stats">
-                                <tr>
-                                    <td class="pokemon__energy">☻</td>
-                                    <td class="pokemon__attack_description pokemon__attack_description--center"><span class="label">Gnaw</span></td>
-                                    <td class="pokemon__damage">10</td>
-                                </tr>
-                            </table>
-                            <table class="pokemon__stats">
-                                <tr>
-                                    <td class="pokemon__energy">☻ ☻</td>
-                                    <td class="pokemon__attack_description"><span class="label">Thundershock</span> <span class="labeltext">Flip a coin. If tails, ${randomPokemon.name} does 10 damage to itself.</span></td>
-                                    <td class="pokemon__damage">20</td>
-                                </tr>
-                            </table>
-                            <table class="pokemon__costs">
-                                <tr class="pokemon__cost_headers">
-                                    <td>weakness</td>
-                                    <td>resistance</td>
-                                    <td>retreat cost</td>
-                                </tr>
-                                <tr class="pokemon__cost_icons">
-                                    <td>☻</td>
-                                    <td></td>
-                                    <td>☻</td>
-                                </tr>
-                            </table>
-                            <ul>
-                                <li class="italicize">I'm writing about pokemons.</li>
-                                <li class="pokemon__copyrights"><br><span class="strong">Illus Mitsuhiro Arita</strong>  <span class="medium">©1995, 96, 98 Nintendo Creatures, GAMEFREAK ©1999 Wizards</span> <span class="strong">58/102 ●</strong></li>       
-                            </ul>
+            if (tcgCard) {
+                pokemonElement.innerHTML = `
+                    <div class="pokemon__card-inner">
+                        <div class="pokemon__background pokemon__background--${backgroundClass}">
+                            <img src="${tcgCard}" class="tcg-card" alt="${randomPokemon.name}">
                         </div>
+                        <img class="pokemon__back is-hidden" src="/assets/back-pokemon-card.png" alt="pokemon card back">
                     </div>
-                    <img class="pokemon__back is-hidden" src="/assets/back-pokemon-card.png" alt="pokemon card back">
-                </div>
-            `;
+                `;
+            } else {
+                pokemonElement.innerHTML = `
+                    <div class="pokemon__card-inner">
+                        <div class="pokemon__background pokemon__background--${backgroundClass} --not-tcg">
+                            <table class="pokemon__header">
+                                <tr>
+                                    <td class="basic" colspan="3">Basic Pokémon</td>
+                                </tr>
+                                <tr>
+                                    <td class="pokemon__name">${randomPokemon.name} ${randomPokemon.type}</td>
+                                    <td class="pokemon__hp">${randomPokemon.hp} HP</td>
+                                    <td class="typesign"> ☻ </td>
+                                </tr>
+                            </table>
+                            <img class="pokemon__image" src="${randomPokemon.image}" alt="${randomPokemon.name}">
+                            <br>
+                            <div class="pokemon__info">
+                                <p class="pokemon__description">This is a pokemon description.</p>
+                                <table class="pokemon__stats">
+                                    <tr>
+                                        <td class="pokemon__energy">☻</td>
+                                        <td class="pokemon__attack_description pokemon__attack_description--center"><span class="label">Gnaw</span></td>
+                                        <td class="pokemon__damage">10</td>
+                                    </tr>
+                                </table>
+                                <table class="pokemon__stats">
+                                    <tr>
+                                        <td class="pokemon__energy">☻ ☻</td>
+                                        <td class="pokemon__attack_description"><span class="label">Thundershock</span> <span class="labeltext">Flip a coin. If tails, ${randomPokemon.name} does 10 damage to itself.</span></td>
+                                        <td class="pokemon__damage">20</td>
+                                    </tr>
+                                </table>
+                                <table class="pokemon__costs">
+                                    <tr class="pokemon__cost_headers">
+                                        <td>weakness</td>
+                                        <td>resistance</td>
+                                        <td>retreat cost</td>
+                                    </tr>
+                                    <tr class="pokemon__cost_icons">
+                                        <td>☻</td>
+                                        <td></td>
+                                        <td>☻</td>
+                                    </tr>
+                                </table>
+                                <ul>
+                                    <li class="italicize">I'm writing about pokemons.</li>
+                                    <li class="pokemon__copyrights"><br><span class="strong">Illus Mitsuhiro Arita</strong>  <span class="medium">©1995, 96, 98 Nintendo Creatures, GAMEFREAK ©1999 Wizards</span> <span class="strong">58/102 ●</strong></li>       
+                                </ul>
+                            </div>
+                        </div>
+                        <img class="pokemon__back is-hidden" src="/assets/back-pokemon-card.png" alt="pokemon card back">
+                    </div>
+                `;
+            }
 
-        }
-        randomPokemonContainer.appendChild(pokemonElement);
+            randomPokemonContainer.appendChild(pokemonElement);
+            resolve();
+        }));
     }
+
+    await Promise.all(cardPromises);
 
     shuffleCards();
 
